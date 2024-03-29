bf30164ba99eb1dc87f4fa34300de622f5dd0d2f
pdkkg <kushwahagautam24@gmail.com>
2024-03-29 23:27:21 +0530

    43 number completed today

---
+++ set.py
@@ -0,0 +1,16 @@
+# operation in python set: union(|), intersection(&), difference(-), symmetric(^)
+
+# to define the operations, define the sets
+
+set1 = {0, 2, 4, 6, 8}
+set2 ={1, 2, 3, 4, 5}
+
+# union of sets
+
+print(" UNION of sets:", set1 | set2)
+
+print(" Intersection of sets:", set1 & set2)
+
+print(" difference of sets:", set1 - set2)
+
+print(" Symmetric of sets:", set1 ^ set2)
